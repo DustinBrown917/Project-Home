0e268fd1fa3a5c7c7458718d355b80f88af55a2e
DustinBrown917 <43157195+DustinBrown917@users.noreply.github.com>
2019-01-27 13:30:23 -0500

    More Sunday Changes

---
+++ Project Home/Assets/_Scripts/SceneTransitioner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HOME
+{
+    public class SceneTransitioner : MonoBehaviour
+    {
+        private static SceneTransitioner _instance;
+        public static SceneTransitioner Instance { get { return _instance; } }
+
+        [SerializeField] private float transitionFadeDuration;
+        private CanvasGroup cg;
+        private Coroutine cr_Transitoning;
+
+        private void Awake()
+        {
+            if(_instance == null)
+            {
+                _instance = this;
+                cg = GetComponent<CanvasGroup>();
+                DontDestroyOnLoad(this.gameObject);
+            } else
+            {
+                Destroy(this.gameObject);
+            }
+
+
+        }
+
+        private void OnDestroy()
+        {
+            if(Instance == this)
+            {
+                _instance = null;
+            }
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        public void LoadScene(string str)
+        {
+            CoroutineManager.BeginCoroutine(SceneTransition(str), ref cr_Transitoning, this);
+        }
+
+        private IEnumerator SceneTransition(string str)
+        {
+            float t = 0;
+
+            while (t < transitionFadeDuration)
+            {
+                t += Time.deltaTime;
+                cg.alpha = Mathf.Lerp(0, 1, t / transitionFadeDuration);
+                yield return null;
+            }
+
+            SceneManager.LoadScene(str);
+            yield return  new WaitForSeconds(0.1f);
+            t = 0;
+
+            while (t < transitionFadeDuration)
+            {
+                t += Time.deltaTime;
+                cg.alpha = Mathf.Lerp(1, 0, t / transitionFadeDuration);
+                yield return null;
+            }
+        }
+    }
+}
+
